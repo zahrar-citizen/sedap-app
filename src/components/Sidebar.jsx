a53a450ff5261f60273225f746bbbfa94d28bf57
zahrar-citize <zahra23si@mahasiswa.pcr.ac.id>
2025-05-25 22:01:38 +0700

    ubah logo sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
             {/* Logo */}
             <div id="sidebar-logo" className="flex flex-col">
                 <span id="logo-title" className="font-poppins-extrabold text-[48px] text-gray-900">
-                    Sedap <b id="logo-dot" className="text-hijau">.</b>
+                    Mantap <b id="logo-dot" className="text-hijau">.</b>
                 </span>
                 <span id="logo-subtitle" className="font-semibold text-gray-400">Modern Admin Dashboard</span>
             </div>
